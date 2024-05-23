b24ebdb35dbd384afe916ef1389e58754ce93cff
Majid Mohammed <majidmohammed044@gmail.com>
2024-05-23 16:03:26 +0000

    header interactivity

---
+++ interactivity.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
+// The end of heading interactiveness....
+
